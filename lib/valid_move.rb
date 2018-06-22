34001fdab8f48775db6f7a91ee69995e0b29530e
Adam Taitano <adam.taitano@gmail.com>
2018-06-22 18:45:16 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if !position_taken?(board, index)
+  if (index)
+  elsif !position_taken?(board, index)
     true
   else
     false
